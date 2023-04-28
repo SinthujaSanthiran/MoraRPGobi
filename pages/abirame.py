03b2aa4cffd37b7fff3ecb3bf35762cfe1778b28
Jude Sajith <judesajith.aj@gmail.com>
2023-04-29 00:39:17 +0530

    ssss

--- pages/abirame.py
+++ pages/abirame.py
@@ -1,56 +1,25 @@
 import os
-import json
 import streamlit as st
-from googlesearch import search
 import requests
-from bs4 import BeautifulSoup
 
-def google_search(query):
-    search_results = []
-    for url in search(query,sleep_interval=5, num_results=20):
-        search_results.append(url)
-    return search_results
+def save_html_to_file(content, filename):
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(content)
 
-# def extract_business_data(url):
-#     response = requests.get(url)
-#     soup = BeautifulSoup(response.content, "html.parser")
+st.title("Google Search Results HTML Downloader")
+search_results_url = st.text_input("Enter the search results URL:", "")
 
-#     business_name = soup.find("h1", class_="section-hero-header-title-title").get_text(strip=True) if soup.find("h1", class_="section-hero-header-title-title") else None
-#     business_rating = soup.find("span", class_="section-star-display").get_text(strip=True) if soup.find("span", class_="section-star-display") else None
-#     business_reviews = soup.find("button", class_="section-reviewchart-numreviews").get_text(strip=True) if soup.find("button", class_="section-reviewchart-numreviews") else None
+if st.button("Download HTML"):
+    try:
+        response = requests.get(search_results_url)
+        if response.status_code == 200:
+            if not os.path.exists("data"):
+                os.makedirs("data")
 
-#     return {
-#         "name": business_name,
-#         "rating": business_rating,
-#         "reviews": business_reviews,
-#     }
-
-def save_to_file(data, filename):
-    with open(filename, "w") as f:
-        json.dump(data, f, indent=2)
-
-st.title("Google Business Page Scraper")
-keyword = st.text_input("Enter a keyword:", "")
-
-if st.button("Search and Save"):
-    st.write("Searching for businesses related to:", keyword)
-    search_query = f"{keyword}"
-
-    results = google_search(search_query)
-    business_data = []
-
-    # for result in results:
-    #     try:
-    #         data = extract_business_data(result)
-    #         if data["name"]:
-    #             business_data.append(data)
-    #     except Exception as e:
-    #         st.write(f"Error while processing {result}: {e}")
-
-    if results:
-        if not os.path.exists("data"):
-            os.makedirs("data")
-        save_to_file(business_data, f"data/{keyword}_business_data.json")
-        st.write("Data saved in 'data' folder.")
-    else:
-        st.write("No businesses found.")
+            file_name = "search_results.html"
+            save_html_to_file(response.text, f"data/{file_name}")
+            st.write(f"HTML content saved as '{file_name}' in the 'data' folder.")
+        else:
+            st.write(f"Error: Failed to fetch the content. Status code: {response.status_code}")
+    except Exception as e:
+        st.write(f"Error while downloading HTML: {e}")
