f3da6cbef4b8519b3f6b2b89adc9e18b70410c2b
Jude Sajith <judesajith.aj@gmail.com>
2023-06-08 23:56:21 +0530

    ssss

---
+++ pages/chatbot.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from pathlib import Path
+from llama_index import download_loader
+from pandasai.llm.openai import OpenAI
+
+# Initialize the AI
+llm = OpenAI()
+PandasAIReader = download_loader("PandasAIReader")
+loader = PandasAIReader(llm=llm)
+
+# Define the path to your CSV file
+file_path = Path('data/Dataset.csv')
+
+# Read the CSV file into a pandas DataFrame
+df = pd.read_csv(file_path)
+
+# Display the DataFrame
+st.write(df)
+
+# Get a question from the user
+query = st.text_input("Enter your question")
+
+if query:
+    response = loader.run_pandas_ai(df, query, is_conversational_answer=True)
+    st.write(response)
