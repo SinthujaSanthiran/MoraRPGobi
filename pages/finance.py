be00de3bcbb8488681c8d28bd0bef7975d6bc485
Jude Sajith <judesajith.aj@gmail.com>
2023-06-08 22:41:36 +0530

    ssss

---
+++ pages/finance.py
@@ -0,0 +1,28 @@
+from llama_index import download_loader
+import os
+import time
+import streamlit as st
+import requests
+from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
+import openai
+
+
+
+
+AsyncWebPageReader = download_loader("AsyncWebPageReader")
+
+loader = AsyncWebPageReader()
+documents = loader.load_data(urls=['https://www.thepythoncode.com/article/extract-google-trends-data-in-python','https://llamahub.ai/'])
+
+
+index = GPTSimpleVectorIndex.from_documents(documents)
+index.save_to_disk('index.json')
+index = GPTSimpleVectorIndex.load_from_disk('index.json')
+if "index" not in st.session_state:
+    st.session_state.index = index
+    st.info("Index created")
+
+questi = st.text_input("Enter question")
+if st.button("ask bot"):
+    reso = st.session_state.index.query(questi)
+    st.write(reso)
